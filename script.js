f92a3539c1007d7e40b91068f6a759ad74bd672a
Arman Darkahi <arman.darkahi@bonniernews.se>
2019-01-23 15:37:38 +0100

    objekts o grejer

--- script.js
+++ script.js
@@ -35,42 +35,68 @@
 // document.querySelector(".showDiv").innerHTML = (fullName.repeat(3999))
 
 
-let clickCount = 0
+// let clickCount = 0
+//
+// let incrementCount = () => {
+//   clickCount += 1
+//   document.querySelector(".output").innerHTML = clickCount
+//
+// }
+//
+// document.querySelector(".myButton").onclick = incrementCount
+//
+// let fruits = [ "apple", "orange", "lemon"]
+//
+//   console.log(fruits[2])
+//
+// fruits.forEach (fruitName => {
+//   console.log(fruitName)
+// })
+//
+//
+// let newsTitles = ["Hotet mot handbollens framtid i Stockholm", "jo, placebo funkar – även när man vet att det är fejk", "Vilken version av dig umgås dina vänner med i kväll?"]
+//
+// newsTitles.forEach (nyheter => {
+//   console.log(nyheter)
+//
+// })
+//
+//
+// let articlesDiv = document.querySelector(".demo")
+//
+// newsTitles.push("arman test")
+// newsTitles.forEach(title => {
+//
+//   articlesDiv.innerHTML += `<h2>${title}</h2>`
+// })
 
-let incrementCount = () => {
-  clickCount += 1
-  document.querySelector(".output").innerHTML = clickCount
-
-}
-
-document.querySelector(".myButton").onclick = incrementCount
-
-let fruits = [ "apple", "orange", "lemon"]
-
-  console.log(fruits[2])
-
-fruits.forEach (fruitName => {
-  console.log(fruitName)
-})
 
 
-let newsTitles = ["Hotet mot handbollens framtid i Stockholm", "jo, placebo funkar – även när man vet att det är fejk", "Vilken version av dig umgås dina vänner med i kväll?"]
 
-newsTitles.forEach (nyheter => {
-  console.log(nyheter)
+let newsArticle = [
+  {
+    title: "Arman är bäst",
+    subtitle: "blah blah",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    title: "Arman är bäst igen",
+    subtitle: "blah blah again",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-})
-
+  }
+]
 
 let articlesDiv = document.querySelector(".demo")
 
-newsTitles.push("arman test")
+newsArticle.forEach(article =>{
+  articlesDiv.innerHTML += `
+    <h2>${article.title}</h2>
+    <h3>${article.subtitle}</h3>
+    <p>${article.description}</p>
+  `
 
-newsTitles.forEach(title => {
-
-  articlesDiv.innerHTML += `<h2>${title}</h2>`
-
-
-
+  console.log(article.title)
+  console.log(article.subtitle)
 
 })
