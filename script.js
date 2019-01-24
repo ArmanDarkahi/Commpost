4d746020132bb1c82ed67566ffa62cbc2c3ed268
Arman Darkahi <arman.darkahi@bonniernews.se>
2019-01-24 12:53:37 +0100

    api koppling instead of hårdkodad

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-
-let articlesAdvertisingArray = [
-  {
-    image: "https://static.adweek.com/adweek.com-prod/wp-content/uploads/2019/01/bk-hamberders-content-2019.png",
-    link: "https://www.adweek.com/brand-marketing/burger-king-trolls-president-trump-plus-how-more-brands-are-responding-to-the-government-shutdown/",
-    headline: "Burger King Trolls President Trump, Plus How More Brands Are Responding to the Government Shutdown",
-    details: "By Katie Richards | 16 hours ago",
-    body: "Sportswear companies are cleaning up national parks",
-
-  },
-  {
-    image: "https://static.adweek.com/adweek.com-prod/wp-content/uploads/2019/01/bud-light-ingredients-hed-2-2019.jpg",
-    link: "https://www.adweek.com/creativity/for-bud-light-even-announcing-ingredient-and-nutrition-labels-is-a-regal-affair/",
-    headline: "For Bud Light, Even Announcing Ingredient and Nutrition Labels Is a Regal Affair",
-    details: "By Katie Richards | 5 days ago",
-    body: "By royal decree, the brewer takes the lead on voluntary disclosure",
-
-  }
-]
+// Our variables
+let url = "https://newsapi.org/v2/everything?q=advertising&language=en&sortBy=relevancy&pageSize=10&apiKey=e3ce35f448344649a84499d591b27761"
 
 
-articlesAdvertisingArray.forEach(article =>{
-  let articlesAdvertisingArray = document.querySelector(".articleAdvertising")
+// Our main function which handles the news items and adds them to the DOM
+let receivedNews = (newsDataAdvertising) => {
 
-  articlesAdvertisingArray.innerHTML += `
-    <img src="${article.image}" alt="">
-    <h3>${article.headline}</h3>
-    <h4>${article.details}</h4>
-    <p>${article.body}</p>
-  `
+  // newsData is the entire response from the API.
+  // newsData.articles is an array of objects representing each article.
+  // For each article object from the API, we create a new div in HTML.
+  newsDataAdvertising.articles.forEach((article) => {
 
-})
+    // Here we create and add html elements to our html file
+    document.querySelector(".articleAdvertising").innerHTML +=
+      `<div class="news">
+        <a href="${article.url}"><img src="${article.urlToImage}" alt=""></a>
+        <a href="${article.url}">  <h3>${article.title}</h3></a>
+        <h4>${article.author} ${article.publishedAt}</h4>
+        <p>${article.description}</p>
+      </div>`
+
+  })
+}
+
+
+// Fetch is a built in function in JavaScript, it gets the data from the API and transforms it into JavaScript objects – JSON data.
+// These lines call the API, makes sure we get the response as JSON, and finally passes the result to the function we defined above, receivedNews.
+fetch(url)
+  .then(response => response.json())
+  .then(receivedNews)
