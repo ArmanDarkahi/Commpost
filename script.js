cfc57552ad22b7ee3cd72e4a7235ce2cc048a3c9
Arman Darkahi <arman.darkahi@bonniernews.se>
2019-01-24 14:24:23 +0100

    stuff

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Api stuff
 // Our variables
-let url = "https://newsapi.org/v2/everything?q=advertising&language=en&sortBy=relevancy&pageSize=10&apiKey=e3ce35f448344649a84499d591b27761"
+let urlAdvertising = "https://newsapi.org/v2/everything?q=advertising&language=en&sortBy=relevancy&pageSize=10&apiKey=e3ce35f448344649a84499d591b27761"
 
 
 // Our main function which handles the news items and adds them to the DOM
-let receivedNews = (newsDataAdvertising) => {
+let receivedNewsAdvertising = (newsDataAdvertising) => {
 
   // newsData is the entire response from the API.
   // newsData.articles is an array of objects representing each article.
@@ -37,6 +37,81 @@ let receivedNews = (newsDataAdvertising) => {
 
 // Fetch is a built in function in JavaScript, it gets the data from the API and transforms it into JavaScript objects – JSON data.
 // These lines call the API, makes sure we get the response as JSON, and finally passes the result to the function we defined above, receivedNews.
-fetch(url)
+fetch(urlAdvertising)
   .then(response => response.json())
-  .then(receivedNews)
+  .then(receivedNewsAdvertising)
+
+//Marketing
+  let urlMarketing = "https://newsapi.org/v2/everything?q=marketing&language=en&sortBy=relevancy&pageSize=10&apiKey=e3ce35f448344649a84499d591b27761"
+
+  let receivedNewsMarketing = (newsDataMarketing) => {
+
+    newsDataMarketing.articles.forEach((article) => {
+
+      // Here we create and add html elements to our html file
+      document.querySelector(".articleMarketing").innerHTML +=
+        `<div class="news">
+          <a href="${article.url}"><img src="${article.urlToImage}" alt=""></a>
+          <a href="${article.url}">  <h3>${article.title}</h3></a>
+          <h4>${article.author} ${moment(article.publishedAt).format('MMMM Do YYYY, h:mm a')}</h4>
+          <p>${article.description}</p>
+        </div>`
+
+    })
+  }
+
+
+  fetch(urlMarketing)
+    .then(response => response.json())
+    .then(receivedNewsMarketing)
+
+
+
+//FAANG
+
+  let urlFaang = "https://newsapi.org/v2/everything?language=en&q=facebook%20OR%20amazon%20OR%20apple%20OR%20netflix%20OR%20google%20&sortBy=relevancy&pageSize=10&apiKey=e3ce35f448344649a84499d591b27761"
+  let receivedNewsFaang = (newsDataFaang) => {
+
+    newsDataFaang.articles.forEach((article) => {
+
+      // Here we create and add html elements to our html file
+      document.querySelector(".articleFaang").innerHTML +=
+        `<div class="news">
+          <a href="${article.url}"><img src="${article.urlToImage}" alt=""></a>
+          <a href="${article.url}">  <h3>${article.title}</h3></a>
+          <h4>${article.author} ${moment(article.publishedAt).format('MMMM Do YYYY, h:mm a')}</h4>
+          <p>${article.description}</p>
+        </div>`
+
+    })
+  }
+
+
+  fetch(urlFaang)
+    .then(response => response.json())
+    .then(receivedNewsFaang)
+
+
+    //Stocks
+
+    let urlStocks = "https://newsapi.org/v2/everything?q?stocks&language=en&sources=cnbc&sortBy=relevancy&pageSize=10&apiKey=e3ce35f448344649a84499d591b27761"
+    let receivedNewsStocks = (newsDataStocks) => {
+
+      newsDataStocks.articles.forEach((article) => {
+
+        // Here we create and add html elements to our html file
+        document.querySelector(".articleStock").innerHTML +=
+          `<div class="news">
+            <a href="${article.url}"><img src="${article.urlToImage}" alt=""></a>
+            <a href="${article.url}">  <h3>${article.title}</h3></a>
+            <h4>${article.author} ${moment(article.publishedAt).format('MMMM Do YYYY, h:mm a')}</h4>
+            <p>${article.description}</p>
+          </div>`
+
+      })
+    }
+
+
+    fetch(urlStocks)
+      .then(response => response.json())
+      .then(receivedNewsStocks)
